a441a4e8b54e89d5410bdbfa091adb1ec7eefbbd
crischo <crismasteroz777@gmail.com>
2018-08-24 19:46:58 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'servidor';
+  posts = [];
+
+  constructor(private dataService: DataService){
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    })
+  }
+
+  
 }
